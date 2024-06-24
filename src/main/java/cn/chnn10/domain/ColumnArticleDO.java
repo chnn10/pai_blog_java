3952d246217381d2ac473cc50189a0491b0518e8
chnn10 <46521173+chnn10@users.noreply.github.com>
2024-06-24 19:16:48 +0800

    Create ColumnArticleDO.java

---
+++ src/main/java/cn/chnn10/domain/ColumnArticleDO.java
@@ -0,0 +1,27 @@
+package cn.chnn10.domain;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("column_article")
+public class ColumnArticleDO extends BaseDO {
+    private static final long serialVersionUID = -2372103913090667453L;
+
+    private Long columnId;
+
+    private Long articleId;
+
+    /**
+     * 顺序，越小越靠前
+     */
+    private Integer section;
+
+    /**
+     * 专栏类型：免费、登录阅读、收费阅读等
+     *
+     */
+    private Integer readType;
+}
