e378980f94dfbf7c20dccbe58fe1ee97eae18dc5
chnn10 <46521173+chnn10@users.noreply.github.com>
2024-06-24 19:18:06 +0800

    Update CommentDO.java

--- src/main/java/cn/chnn10/domain/CommentDO.java
+++ src/main/java/cn/chnn10/domain/CommentDO.java
@@ -1,1 +1,43 @@
+package cn.chnn10.domain;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("comment")
+public class CommentDO extends BaseDO {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 文章ID
+     */
+    private Long articleId;
+
+    /**
+     * 用户ID
+     */
+    private Long userId;
+
+    /**
+     * 评论内容
+     */
+    private String content;
+
+    /**
+     * 父评论ID
+     */
+    private Long parentCommentId;
+
+    /**
+     * 顶级评论ID
+     */
+    private Long topCommentId;
+
+    /**
+     * 0未删除 1 已删除
+     */
+    private Integer deleted;
+}
