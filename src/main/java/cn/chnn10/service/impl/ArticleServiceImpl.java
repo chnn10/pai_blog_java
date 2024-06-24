a25d8bd22ca9c2ebe51d36ebcc6e755da0ba880f
chnn10 <46521173+chnn10@users.noreply.github.com>
2024-06-24 20:01:19 +0800

    Update ArticleServiceImpl.java

--- src/main/java/cn/chnn10/service/impl/ArticleServiceImpl.java
+++ src/main/java/cn/chnn10/service/impl/ArticleServiceImpl.java
@@ -1,15 +1,28 @@
 package cn.chnn10.service.impl;
 
+import cn.chnn10.conveter.ArticleConverter;
+import cn.chnn10.domain.ArticleDO;
 import cn.chnn10.params.request.ArticlePostReq;
 import cn.chnn10.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Slf4j
 @Service
 public class ArticleServiceImpl implements ArticleService {
     @Override
     public Long saveArticle(ArticlePostReq req, Long author) {
+        ArticleDO articleDO = ArticleConverter.toArticleDo(req, author);
+        return null;
+    }
+
+    private Long insertArticle(ArticlePostReq req, String content, Set<Long> tags) {
+        return null;
+    }
+
+    private Long updateArticle(ArticlePostReq req, String content, Set<Long> tags) {
         return null;
     }
 }
