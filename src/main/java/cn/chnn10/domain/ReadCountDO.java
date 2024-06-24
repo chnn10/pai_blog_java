0fac1e4d9f356a8f9cef05b5d5e44f192c2f4ead
chnn10 <46521173+chnn10@users.noreply.github.com>
2024-06-24 19:25:53 +0800

    Create ReadCountDO.java

---
+++ src/main/java/cn/chnn10/domain/ReadCountDO.java
@@ -0,0 +1,28 @@
+package cn.chnn10.domain;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+@TableName("read_count")
+public class ReadCountDO extends BaseDO {
+    /**
+     * 文档ID（文章/评论）
+     */
+    private Long documentId;
+
+    /**
+     * 文档类型：1-文章，2-评论
+     */
+    private Integer documentType;
+
+    /**
+     * 计数
+     */
+    private Integer cnt;
+
+}
